47d904617ba4fb8b85fa40abd73bcad6f74d96cc
Parkerrr <jason@parkerrr.com>
2014-11-28 16:12:26 -0500

    migrated tweet model data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128191439) do
+ActiveRecord::Schema.define(version: 20141128210600) do
+
+  create_table "tweets", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "provider"
