2d08fb71d1478a4898c3ae44cb2f013b7daa734c
Doug Steinberg <dstein-phins@hotmail.com>
2014-12-01 16:09:10 +0000

    fixed missing ':' in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  get 'auth/:provider/callback', to 'sessions#create'
+  get '/auth/:provider/callback', to: 'sessions#create'
   resources :tweets, only: [:create]
 end
