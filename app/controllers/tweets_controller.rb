d533ed4b7813152ab9198752755862c1220fd1e6
Doug Steinberg <dstein-phins@hotmail.com>
2014-12-01 19:39:50 -0500

    can post tweets and see last 5 in timeline

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,49 +1,21 @@
 class TweetsController < ApplicationController
-  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
-
-  respond_to :html
-
-  def index
-    @tweet = Tweet.new
-    @tweets = Tweet.all
-    respond_with(@tweets)
-  end
-
-  def show
-    respond_with(@tweet)
-  end
-
-  def new
-    @tweet = Tweet.new
-    respond_with(@tweet)
-  end
-
-  def edit
-  end
 
   def create
-    @tweet = Tweet.new(tweet_params)
-    @tweet.user_id = current_user.id
-    @tweet.save
-    respond_with(@tweet)
-  end
-
-  def update
-    @tweet.update(tweet_params)
-    respond_with(@tweet)
-  end
-
-  def destroy
-    @tweet.destroy
-    respond_with(@tweet)
+    @user = current_user
+    @tweet = Tweet.new(tweet_params.merge(user_id: @user.id))
+    if @tweet.valid? 
+      @tweet.post_to_twitter
+      flash[:success] = "Your tweet has been sent!"
+      redirect_to root_path
+    else
+      flash[:error] = "The tweet can't be empty."
+      render 'home/index'
+    end
   end
 
   private
-    def set_tweet
-      @tweet = Tweet.find(params[:id])
-    end
 
     def tweet_params
-      params.require(:tweet).permit(:user_id, :body)
+      params.require(:tweet).permit(:body)
     end
 end
